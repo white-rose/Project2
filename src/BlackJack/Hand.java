b2221a62e2880570584164e4a5b068abd693550c
Nathan Nguyen <nathanthainguyen@gmail.com>
2018-03-09 02:53:24 -0800

    Added too String for blackjack

--- src/BlackJack/Hand.java
+++ src/BlackJack/Hand.java
@@ -17,4 +17,14 @@ public class Hand {
         return handCountTotal;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+
+        return "The cards in the hand are the following : \n" + sb.toString();
+    }
 }
